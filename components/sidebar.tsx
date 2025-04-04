af7f918ec35042ca9470de852f4157bb566e8d60
Teerakan Otklan <161741743+teerakanotk@users.noreply.github.com>
2025-04-04 17:44:40 +0700

    Initial commit

---
+++ components/sidebar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function Sidebar() {
+  return (
+    <aside className="border-grid fixed top-14 z-30 hidden w-full shrink-0 border-r">
+      <nav>Sidebar</nav>
+    </aside>
+  );
+}
