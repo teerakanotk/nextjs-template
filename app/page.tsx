d03fda989ed3a22b4430bee6879e553774624325
teerakanotk <teerakan.otk@gmail.com>
2025-04-05 16:37:30 +0700

    Update: Remove Lorem message

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,3 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid maiores
-      molestias nesciunt accusamus mollitia quia quod, est, dolores incidunt ea
-      dolorem saepe iste, eos voluptatum temporibus eaque? Optio commodi, fuga
-      eos repellendus architecto veniam possimus reprehenderit? In explicabo
-      aperiam voluptate libero voluptatibus fugit, quidem eum ipsam vel
-      assumenda quasi amet. Assumenda pariatur id dignissimos excepturi optio
-      itaque corrupti praesentium sint sit, eos vitae cupiditate nam nesciunt ab
-      aliquam, nemo ullam officia repudiandae quibusdam dolore dicta eius nisi
-      minima a! Corrupti ipsum, assumenda sequi possimus ipsam laboriosam
-      asperiores labore quis non culpa debitis necessitatibus amet ex quam
-      officia doloremque. Neque porro voluptatem, voluptates quis beatae et
-      ratione alias magni omnis necessitatibus facere pariatur nobis earum
-      dolore voluptatibus aut, distinctio non quaerat aliquid corporis
-      blanditiis quia repellat? Praesentium, consectetur dolores quibusdam
-      repellendus modi quidem? Provident, quo ad natus, fuga vero voluptas
-      magnam molestias perferendis, placeat fugiat numquam veritatis in sapiente
-      necessitatibus amet?
-    </div>
-  );
+export default function HomePage() {
+  return <main>HomePage</main>;
 }
